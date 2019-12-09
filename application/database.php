1833ebb35c95beaedaba78a528591048ca1e83af
l58561558 <34786431+l58561558@users.noreply.github.com>
2019-12-09 18:03:55 +0800

    save config

--- application/database.php
+++ application/database.php
@@ -16,13 +16,13 @@ return [
     // 数据库类型
     'type'            => Env::get('database.type', 'mysql'),
     // 服务器地址
-    'hostname'        => Env::get('database.hostname', '127.0.0.1'),
+    'hostname'        => Env::get('database.hostname', 'localhost'),
     // 数据库名
-    'database'        => Env::get('database.database', 'nvzhuang'),
+    'database'        => Env::get('database.database', 'scdaili'),
     // 用户名
-    'username'        => Env::get('database.username', 'root'),
+    'username'        => Env::get('database.username', 'scdaili'),
     // 密码
-    'password'        => Env::get('database.password', 'root'),
+    'password'        => Env::get('database.password', 'scdaili888'),
     // 端口
     'hostport'        => Env::get('database.hostport', ''),
     // 连接dsn
