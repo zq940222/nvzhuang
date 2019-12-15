e9041f29b0868afe01c9255584bf27ab4408ec15
l58561558 <34786431+l58561558@users.noreply.github.com>
2019-12-15 19:16:43 +0800

    Update common.php

--- application/api/common.php
+++ application/api/common.php
@@ -1,1 +1,10 @@
 <?php
+/**
+ * 生成数据返回值
+ */
+function jsonReturn($msg,$status = -1,$data = []){
+	if(isset($data['status']))return json_encode($data);
+	$rs = ['status'=>$status,'msg'=>$msg];
+	if(!empty($data))$rs['data'] = $data;
+	return json_encode($rs);
+}
