75fd3284acd6f04f5fe823e91ecad30df659ea39
l58561558 <34786431+l58561558@users.noreply.github.com>
2019-12-16 21:04:37 +0800

    update
    2019/12/16 21:04

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -13,7 +13,7 @@ use think\Validate;
  */
 class User extends Api
 {
-    protected $noNeedLogin = ['login', 'mobilelogin', 'register', 'resetpwd', 'changeemail', 'changemobile', 'third', 'apply_info'];
+    protected $noNeedLogin = ['login', 'mobilelogin', 'register', 'resetpwd', 'changeemail', 'changemobile', 'third', 'apply_info','apply_agent','get_http_host'];
     protected $noNeedRight = '*';
 
     public function _initialize()
@@ -21,6 +21,10 @@ class User extends Api
         parent::_initialize();
     }
 
+    public function get_http_host()
+    {
+        $this->success('请求成功', $this->request->server()['HTTP_HOST']);
+    }
     /**
      * 获取代理申请基本信息
      *
@@ -30,7 +34,7 @@ class User extends Api
     {
         $user_id = $this->request->request('user_id');
         if(!$user_id) {
-            $this->error(__('Invalid parameters'), null, -1);
+            $this->error(__('无效的参数 : user_id'), null, -1);
         }
         $pay_config = db('config')
         ->field('name as "key",title,value')
@@ -65,6 +69,59 @@ class User extends Api
     }
 
     /**
+     * 申请代理
+     *
+     * @param int $superior_id  用户id
+     * @param string $agency_id  代理等级
+     * @param string $name  姓名
+     * @param string $mobile  手机号
+     * @param string $captcha  验证码
+     * @param string $password  密码
+     * @param string $wx  微信账号
+     * @param string $id_card  身份证号
+     * @param int $pay_type  打款方式:1=支付宝,2=银行卡
+     * @param string $pay_money  打款金额
+     * @param string $bank_account  打款帐号
+     * @param string $pay_time  打款日期
+     * @param string $avatar  头像
+     * @param string $pay_certificate_images  打款凭证
+     */
+    public function apply_agent()
+    {
+        $data['superior_id'] = $this->request->request('superior_id');
+        $data['agency_id'] = $this->request->request('agency_id');
+        $data['name'] = $this->request->request('name');
+        $data['mobile'] = $this->request->request('mobile');
+        // $data['captcha'] = $this->request->request('captcha');
+        $data['password'] = $this->request->request('password');
+        $data['wx'] = $this->request->request('wx');
+        $data['id_card'] = $this->request->request('id_card');
+        $data['pay_type'] = $this->request->request('pay_type');
+        // $data['pay_money'] = $this->request->request('pay_money');
+        $data['bank_account'] = $this->request->request('bank_account');
+        $data['pay_time'] = $this->request->request('pay_time');
+        $data['avatar'] = $this->request->request('avatar');
+        $data['pay_certificate_images'] = $this->request->request('pay_certificate_images');
+        foreach ($data as $key => $value) {
+            if(!$value) {
+                $this->error(__('无效的参数 : '.$key), null, -1);
+            }
+        }
+        /****验证码验证****/
+
+        /****验证码验证end****/
+        $data['createtime'] = time();
+        $res = db('agent_apply')->insert($data);
+        if($res) {
+            $this->success('提交成功');
+        }else{
+            $this->success('创建数据是败', $res, -2);
+        }
+
+        
+    }
+
+    /**
      * 会员登录
      *
      * @param string $account  账号
@@ -75,7 +132,7 @@ class User extends Api
         $account = $this->request->request('account');
         $password = $this->request->request('password');
         if (!$account || !$password) {
-            $this->error(__('Invalid parameters'));
+            $this->error(__('无效的参数'), null, -1);
         }
         $ret = $this->auth->login($account, $password);
         if ($ret) {
