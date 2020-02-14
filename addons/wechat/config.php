947df1afb71388c39df855900edaa21bf620a1ce
l58561558 <34786431+l58561558@users.noreply.github.com>
2020-02-14 12:24:33 +0800

    Revert "Merge branch 'master' of https://github.com/zq940222/nvzhuang"
    This reverts commit bc782fb3cf319c336a9e65fb2d11d1326fc991ee, reversing
    changes made to e4e76a017d1f1e7d8cb2a46dc4002b8df05b0b13.

--- addons/wechat/config.php
+++ addons/wechat/config.php
@@ -1,119 +1,114 @@
 <?php
 
-return array (
-  0 => 
-  array (
-    'name' => 'app_id',
-    'title' => 'app_id',
-    'type' => 'string',
-    'content' => 
-    array (
-    ),
-    'value' => 'wx932b4c0e558f490c',
-    'rule' => 'required',
-    'msg' => '',
-    'tip' => '',
-    'ok' => '',
-    'extend' => '',
-  ),
-  1 => 
-  array (
-    'name' => 'secret',
-    'title' => 'secret',
-    'type' => 'string',
-    'content' => 
-    array (
-    ),
-    'value' => 'fab6f6412464ba9083860a5f393cc28c',
-    'rule' => 'required',
-    'msg' => '',
-    'tip' => '',
-    'ok' => '',
-    'extend' => '',
-  ),
-  2 => 
-  array (
-    'name' => 'token',
-    'title' => 'token',
-    'type' => 'string',
-    'content' => 
-    array (
-    ),
-    'value' => 'qscong921026',
-    'rule' => 'required',
-    'msg' => '',
-    'tip' => '',
-    'ok' => '',
-    'extend' => '',
-  ),
-  3 => 
-  array (
-    'name' => 'aes_key',
-    'title' => 'aes_key',
-    'type' => 'string',
-    'content' => 
-    array (
-    ),
-    'value' => 'UOLWLFSZbz1mrNCCans1zj3oRbs1n1Cjmaovn2NYQJO',
-    'rule' => 'required',
-    'msg' => '',
-    'tip' => '',
-    'ok' => '',
-    'extend' => '',
-  ),
-  4 => 
-  array (
-    'name' => 'debug',
-    'title' => '调试模式',
-    'type' => 'radio',
-    'content' => 
-    array (
-      0 => '否',
-      1 => '是',
-    ),
-    'value' => '1',
-    'rule' => 'required',
-    'msg' => '',
-    'tip' => '',
-    'ok' => '',
-    'extend' => '',
-  ),
-  5 => 
-  array (
-    'name' => 'log_level',
-    'title' => '日志记录等级',
-    'type' => 'select',
-    'content' => 
-    array (
-      'debug' => 'debug',
-      'info' => 'info',
-      'notice' => 'notice',
-      'warning' => 'warning',
-      'error' => 'error',
-      'critical' => 'critical',
-      'alert' => 'alert',
-      'emergency' => 'emergency',
-    ),
-    'value' => 'debug',
-    'rule' => 'required',
-    'msg' => '',
-    'tip' => '',
-    'ok' => '',
-    'extend' => '',
-  ),
-  6 => 
-  array (
-    'name' => 'oauth_callback',
-    'title' => '登录回调',
-    'type' => 'string',
-    'content' => 
-    array (
-    ),
-    'value' => 'http://www.yoursite.com/addons/wechat/index/callback',
-    'rule' => 'required',
-    'msg' => '',
-    'tip' => '',
-    'ok' => '',
-    'extend' => '',
-  ),
+return array(
+    0 =>
+        array(
+            'name'    => 'app_id',
+            'title'   => 'app_id',
+            'type'    => 'string',
+            'content' =>
+                array(),
+            'value'   => 'your appid',
+            'rule'    => 'required',
+            'msg'     => '',
+            'tip'     => '',
+            'ok'      => '',
+            'extend'  => '',
+        ),
+    1 =>
+        array(
+            'name'    => 'secret',
+            'title'   => 'secret',
+            'type'    => 'string',
+            'content' =>
+                array(),
+            'value'   => 'your secret',
+            'rule'    => 'required',
+            'msg'     => '',
+            'tip'     => '',
+            'ok'      => '',
+            'extend'  => '',
+        ),
+    2 =>
+        array(
+            'name'    => 'token',
+            'title'   => 'token',
+            'type'    => 'string',
+            'content' =>
+                array(),
+            'value'   => 'your token',
+            'rule'    => 'required',
+            'msg'     => '',
+            'tip'     => '',
+            'ok'      => '',
+            'extend'  => '',
+        ),
+    3 =>
+        array(
+            'name'    => 'aes_key',
+            'title'   => 'aes_key',
+            'type'    => 'string',
+            'content' =>
+                array(),
+            'value'   => 'your aes_key',
+            'rule'    => 'required',
+            'msg'     => '',
+            'tip'     => '',
+            'ok'      => '',
+            'extend'  => '',
+        ),
+    4 =>
+        array(
+            'name'    => 'debug',
+            'title'   => '调试模式',
+            'type'    => 'radio',
+            'content' =>
+                array(
+                    0 => '否',
+                    1 => '是',
+                ),
+            'value'   => '1',
+            'rule'    => 'required',
+            'msg'     => '',
+            'tip'     => '',
+            'ok'      => '',
+            'extend'  => '',
+        ),
+    5 =>
+        array(
+            'name'    => 'log_level',
+            'title'   => '日志记录等级',
+            'type'    => 'select',
+            'content' =>
+                array(
+                    'debug'     => 'debug',
+                    'info'      => 'info',
+                    'notice'    => 'notice',
+                    'warning'   => 'warning',
+                    'error'     => 'error',
+                    'critical'  => 'critical',
+                    'alert'     => 'alert',
+                    'emergency' => 'emergency',
+                ),
+            'value'   => 'debug',
+            'rule'    => 'required',
+            'msg'     => '',
+            'tip'     => '',
+            'ok'      => '',
+            'extend'  => '',
+        ),
+    6 =>
+        array(
+            'name'    => 'oauth_callback',
+            'title'   => '登录回调',
+            'type'    => 'string',
+            'content' =>
+                array(),
+            'value'   => 'http://www.yoursite.com/addons/wechat/index/callback',
+            'rule'    => 'required',
+            'msg'     => '',
+            'tip'     => '',
+            'ok'      => '',
+            'extend'  => '',
+        ),
 );
