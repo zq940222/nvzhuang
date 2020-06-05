67a5e2a1cf9f2b0d9aeb384ecfe8391f3032d355
l58561558 <34786431+l58561558@users.noreply.github.com>
2020-06-05 18:35:46 +0800

    增加广告管理

---
+++ application/admin/lang/zh-cn/ads/ads.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Desc'       => '描述',
+    'Image'      => '图片',
+    'Createtime' => '添加时间',
+    'Weigh'      => '权重'
+];
---
+++ public/assets/js/backend/ads/ads.js
@@ -0,0 +1,53 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'ads/ads/index' + location.search,
+                    add_url: 'ads/ads/add',
+                    edit_url: 'ads/ads/edit',
+                    del_url: 'ads/ads/del',
+                    multi_url: 'ads/ads/multi',
+                    table: 'ads',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'weigh',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'desc', title: __('Desc')},
+                        {field: 'image', title: __('Image'), events: Table.api.events.image, formatter: Table.api.formatter.image},
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'weigh', title: __('Weigh')},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
